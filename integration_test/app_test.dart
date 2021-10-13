a34e0ae9e107d5de2e3d5437b27ee4b900fc16e0
shravaninw <shravani.s@nativewit.in>
2021-10-13 14:23:26 +0530

    integration testing with increment 156

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -14,58 +14,18 @@ void main() {
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
 
-      // Finds the floating action button to tap on.
-      final Finder fab1 = find.byTooltip('Increment');
+      for (int i = 1; i <= 156; i++) {
+        final Finder fab1 = find.byTooltip('Increment');
 
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab1);
+        // Emulate a tap on the floating action button.
+        await tester.tap(fab1);
 
-      // Trigger a frame.
-      await tester.pumpAndSettle();
+        // Trigger a frame.
+        await tester.pumpAndSettle();
 
-      // Verify the counter increments by 1.
-      expect(find.text('1'), findsOneWidget);
-
-      final Finder fab2 = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab2);
-
-      // Trigger a frame.
-      await tester.pumpAndSettle();
-
-      // Verify the counter increments by 2.
-      expect(find.text('2'), findsOneWidget);
-      final Finder fab3 = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab3);
-
-      // Trigger a frame.
-      await tester.pumpAndSettle();
-
-      // Verify the counter increments by 3.
-      expect(find.text('3'), findsOneWidget);
-      final Finder fab4 = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab4);
-
-      // Trigger a frame.
-      await tester.pumpAndSettle();
-
-      // Verify the counter increments by 4.
-      expect(find.text('4'), findsOneWidget);
-      final Finder fab5 = find.byTooltip('Increment');
-
-      // Emulate a tap on the floating action button.
-      await tester.tap(fab5);
-
-      // Trigger a frame.
-      await tester.pumpAndSettle();
-
-      // Verify the counter increments by 5.
-      expect(find.text('5'), findsOneWidget);
+        // Verify the counter increments by 1.
+        expect(find.text('${i}'), findsOneWidget);
+      }
     });
   });
 }
